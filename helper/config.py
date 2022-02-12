02c6e644f9aa4742e420b07762e742f788bfb9f0
spensonj <69922964+spensonj@users.noreply.github.com>
2022-02-12 20:06:41 +0530

    Update config.py

--- helper/config.py
+++ helper/config.py
@@ -18,7 +18,7 @@ try:
     APP_ID = config("APP_ID", cast=int)
     API_HASH = config("API_HASH")
     BOT_TOKEN = config("BOT_TOKEN")
-    OWNER = config("OWNER_ID", default=1322549723, cast=int)
+    OWNER = config("OWNER_ID", default=943328648, cast=int)
     LOG = config("LOG_CHANNEL", cast=int)
 except Exception as e:
     LOGS.info("Environment vars Missing")
